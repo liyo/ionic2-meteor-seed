ec807d86212f27421e6fc6d8d172b1861e9bb3b5
Pablo Wolochwianski <pablo.wolochwianski@gmail.com>
2016-06-04 20:50:06 -0300

    update to latest libraries

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
 /// <reference path="party.d.ts" />
-/// <reference path="meteor_1.3.d.ts" />
+/// <reference path="globals/meteor/index.d.ts" />
+/// <reference path="globals/zone.js/index.d.ts" />
