227ef7a146472587163e03f1959083eb3f49db6e
Pablo Wolochwianski <pablo.wolochwianski@gmail.com>
2016-06-04 21:05:22 -0300

    typings, dependencies cleanup meteor update

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
 /// <reference path="party.d.ts" />
+/// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/meteor/index.d.ts" />
 /// <reference path="globals/zone.js/index.d.ts" />
